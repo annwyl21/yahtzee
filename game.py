c621f20f957349d5bc5dbf2f360b5cb6e27a18c8
Ellen Houghton <ellen.a.ash@gmail.com>
2023-06-29 16:25:25 +0100

    add plan of game development

--- game.py
+++ game.py
@@ -1,1 +1,18 @@
-# game file
+# My command line Yahtzee game - based on the firm family favourite in my house, the dice game of Yahtzee.
+
+class Play_game():
+    pass
+    
+    # define players
+    
+    # take 13 turns - choose auto roll or enter roll
+    
+	# 	choose the result you want to keep from the calculator scores 
+ 
+	# 	set the computer to choose the result it wants to keep from the calculator scores
+ 
+    # call the apply score
+    
+# write a scoreboard total function
+    
+    # determine winner
---
+++ tests/test_game.py
@@ -0,0 +1,7 @@
+import pytest
+from game import Play_game
+
+@pytest.fixture
+def create_player():
+    # j_test = Scoreboard("Joe Test")
+    pass
