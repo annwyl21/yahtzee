9a9d99f886ce7024837fa7943c63494da4c0b717
Ellen Houghton <ellen.a.ash@gmail.com>
2023-07-02 13:31:30 +0100

    need to write tests for scoreboard updating next

--- game.py
+++ game.py
@@ -6,119 +6,77 @@ import random
 
 class Play_game():
     def __init__(self):
-        self.players = {
-            'player1': None,
-            'player2': None,
-            'player3': None,
-            'player4': None,
-            'player5': None,
-            'player6': None
-        }
+        self.players = {'computer', 'player'}
         self._dice_roll = []
-        self.scoreboard_instances = {
-            'player1': None,
-            'player2': None,
-            'player3': None,
-            'player4': None,
-            'player5': None,
-            'player6': None,
-        }
-    
-    def create_scoreboard(self):
-        for num, player in enumerate(self.players.keys()):
-            if player == "computer":
-                self.scoreboard_instances[player] = Scoreboard("computer")
-            else:
-                player = "player" + str(num)
-                self.scoreboard_instances[player] = Scoreboard(player)
-
-    def define_players(self):
-        number_of_players = input("Enter the number of players:")
-        for num in range(int(number_of_players)):
-            player_name = input("Enter player name, type 'computer' to play against the computer:")
-            playerid = "player" + str(num+1)
-            self.players[playerid] = player_name
-        self.players = {instance: player_name for instance, player_name in self.players.items() if player_name != None}
-        self.create_scoreboard()
-        print(f"Players are: {self.players}")
+        self.computer = Scoreboard('computer')
+        self.player = Scoreboard('player')
         
     # maybe have another validation class that does this work    
     def validate_dice_roll_input(self, dice_roll):
         try:
-            dice_roll = [int(dice) for dice in dice_roll.split(',')]
-            if len(dice_roll) == 1:
-                unpack_dice_roll = [int(num) for num in str(dice_roll[0])]
-                if len(unpack_dice_roll) == 5:
-                    dice_roll = unpack_dice_roll
-            return dice_roll
-        except:
-            dice_roll = [int(dice) for dice in dice_roll.split()]
-            if len(dice_roll) == 5:
-                return dice_roll
+            dice_roll_int = [int(dice) for dice in dice_roll.split(',')]
+            if len(dice_roll_int) == 1:
+                return None
             else:
+                return dice_roll_int
+        except:
                 print("ERROR: data entry error")
     
-    def choose_score(self, game_player, results_dict): # make available scores obvious
-        if game_player != 'computer':
-            # find game player in dictionary
-            real_person = [value for key, value in self.players.items() if key == game_player]
-            print(real_person[0], game_player.__str__()) # printing the scoreboard
-            print(real_person, "your dice roll is", self._dice_roll)
-            print("Your score options are:")
-            for num, key, value in enumerate(results_dict.items()):
-                print(num, key, value) #formatted string
-            score_choice = input("Enter the number of the score you want to keep:")
-            score_choice = int(score_choice)
-            score_tuple = list(results_dict.items())[score_choice]
-            print("You chose", score_tuple)
-            return score_tuple
-        else: # computer player always chooses the lowest score != 0 unless the only space in the score dictionary is a score that has a zero then it selectes the first available score
-            computer_player = [key for key, value in self.players.items() if value == 'computer']
-            computer = [value for key, value in self.scoreboard_instances.items() if key == computer_player[0]]
-            computer_scoreboard = computer.getscore_dict()
-            zero_removed = {k: v for k, v in results_dict.items() if v != 0}
-            score_tuple = min(zero_removed.items(), key=lambda x: x[1])
-            while computer_scoreboard[score_tuple[0]] != None:
-                zero_removed.pop(score_tuple[0])
-                score_tuple = min(zero_removed.items(), key=lambda x: x[1])
-                if len(zero_removed) == 0:
-                    score_tuple = min(results_dict.items(), key=lambda x: x[1])
-                    while computer.scoreboard[score_tuple[0]] != None:
-                        results_dict.pop(score_tuple[0])
-                        score_tuple = min(results_dict.items(), key=lambda x: x[1])
-            return score_tuple
+    def choose_score(self, game_player, results_dict):
+        if game_player == 'computer':
+            print(f'Computer results: {results_dict}')
+            print(f'Computer\'s current scoreboard: {self.computer.getscore_dict()}')
+            # 1st available option on scoreboard - computer is not playing to win
+            for key, score in self.computer.getscore_dict().items():
+                if score == None:
+                    score_tuple = key, results_dict[key]
+                    print(f'Computer choose {score_tuple}')
+                    return score_tuple
+        else:
+            print(f'Player\'s current scoreboard: {self.player.getscore_dict()}')
+            print(f'Player\'s results: {results_dict}')
+            # player selects their dice result to add to their scoreboard
+            for num, key in enumerate(results_dict):
+                print(num, key)
+            selected_result = input('Choose the result you wish to add to your scoreboard, type a number:\n')
+            for num, key in enumerate(results_dict):
+                if selected_result == key:
+                    score_tuple = key, results_dict[key]
+                    return score_tuple
 
     def play_game(self):
         print("Welcome to Yahtzee!")
-        self.define_players()
         print("Let's play!")
         
         for turn in range(1, 13):
             for game_player in self.players:
                 if game_player == 'computer':
-                    dice_roll = [random.randrange(1, 7) for i in range(0, 5)]
-                    self._dice_roll = dice_roll
+                    self._dice_roll = [random.randrange(1, 7) for i in range(0, 5)]
                 else:
-                    dice_roll = input("Enter your dice roll, 5,4,3,2,1")
-                    self._dice_roll = self.validate_dice_roll_input(dice_roll)
-            results_dict = Calculator.calculator(dice_roll)
-            # returns a dictionary of score possibilities
+                    dice_roll_input = input("Enter your dice roll, 5,4,3,2,1:\n")
+                    self._dice_roll = self.validate_dice_roll_input(dice_roll_input)
+            
+                results_dict = Calculator(self._dice_roll).calculator()
+                # returns a dictionary of score possibilities
 
-            # send score choices to method to offer choices and return a tuple
-            score_tuple = self.choose_score(game_player, results_dict)
-			
-    # it got confusing when trying to create scoreboard instances to players and players started getting renamed - I need to think on this problem
-    # maybe just have player 1 and the players define the number of players and then always have a computer player. It is 8pm I really need to stop for the day.
+                # Make score choices
+                score_tuple = self.choose_score(game_player, results_dict)
+                if game_player == 'computer':
+                    self.computer.add_score(score_tuple)
+                else:
+                    self.player.add_score(score_tuple)
+
+        # Turn-taking over - grand total scores
+        self.computer.apply_bonus()
+        self.player.apply_bonus()
+
+        self.computer.grand_total()
+        self.player.grand_total()
+
+        print(self.computer.getscore_dict())
+        print(self.player.getscore_dict())
     
-	# 	choose the result you want to keep from the calculator scores 
- 
-	# 	set the computer to choose the result it wants to keep from the calculator scores
- 
-    # call the apply score
-    
-	# call the grand total function
-    
-    # determine winner
+        # determine winner
 
 if __name__ == "__main__":
 	game = Play_game()
--- tests/test_game.py
+++ tests/test_game.py
@@ -14,21 +14,19 @@ def test_user_dice_entry_error_acceptable():
     test_game = Play_game()
     compacted_entry = "65432"
     results = test_game.validate_dice_roll_input(compacted_entry)
-    assert results == [6, 5, 4, 3, 2], "compacted roll failed"
+    assert results == None, "compacted roll failed"
 
 def test_user_entry_acceptable_spaces():
     test_game = Play_game()
     spaced_entry = "5 4 6 1 3"
     results = test_game.validate_dice_roll_input(spaced_entry)
-    assert results == [5, 4, 6, 1, 3], "spaced dice roll failed"
+    assert results == None, "spaced dice roll failed"
 
 def test_choose_score_computer():
     test_game = Play_game()
-    test_game.players = {'player1': 'computer'}
-    test_game.scoreboard_instances = {'player1': Scoreboard("computer")}
     results_dict = {'aces': 1, 'twos': 2, 'threes': 0, 'fours': 4, 'fives': 0, 'sixes': 6, 'three_of_a_kind': 0, 'four_of_a_kind': 0, 'full_house': 0, 'sm_straight': 0, 'lg_straight': 0, 'chance': 13, 'yahtzee': 0}
     results = test_game.choose_score('computer', results_dict)
-    assert results == (('aces', 1)), "computer did not choose the lowest score"
+    assert results == (('aces', 1)), "computer did not choose the first available entry on the blank scoreboard"
 
 def test_choose_score_from_multiple_scores():
     test_game = Play_game()
@@ -37,12 +35,5 @@ def test_choose_score_from_multiple_scores():
     results = test_game.choose_score('computer', results_dict)
     assert results == (('aces', 2)), "computer did not choose the lowest score"
 
-# test for computer choosing the lowest available score given the scoreboard
-def test_choose_score_given_scoreboard():
-    test_game = Play_game()
-    computer = Scoreboard("computer")
-    computer.add_score(('aces', 6))
-    results_dict = {'aces': 2, 'twos': 2, 'threes': 0, 'fours': 4, 'fives': 0, 'sixes': 6, 'three_of_a_kind': 0, 'four_of_a_kind': 0, 'full_house': 0, 'sm_straight': 0, 'lg_straight': 0, 'chance': 13, 'yahtzee': 0}
-    results = test_game.choose_score('computer', results_dict)
-    assert results == (('twos', 2)), "computer did not choose the lowest score"
+# ask Pete about mocking a game to set a portion of the scoreboard
 
