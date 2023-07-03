d95689475e70ee8e954dbc191033fe1314750dee
Ellen Houghton <ellen.a.ash@gmail.com>
2023-07-03 17:20:25 +0100

    write test for grand total

--- tests/test_whole_game.py
+++ tests/test_whole_game.py
@@ -19,3 +19,13 @@ def test_program():
     # assert child.isalive() == False
     # assert child.exitstatus == 0
 
+def test_grand_score():
+    child = pexpect.spawn('python game.py')
+
+    for num in range(1, 14):
+        child.expect('.*Enter your dice roll, 5,4,3,2,1:\r\n')
+        child.sendline("1,3,3,5,5")
+        child.expect('.*Choose the result you wish to add to your scoreboard, type a number:\r\n')
+        child.sendline(str(num))
+    
+    child.expect('.*34')
