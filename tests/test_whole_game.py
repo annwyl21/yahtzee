8e79a5f62225a69ba846967ef2ff6fb08952f982
Ellen Houghton <ellen.a.ash@gmail.com>
2023-07-05 12:31:04 +0100

    investigate missing 21 points!

--- tests/test_whole_game.py
+++ tests/test_whole_game.py
@@ -51,3 +51,74 @@ def test_grand_score_with_larger_score():
         child.sendline(str(num))
     
     child.expect('.*210') #work out the score 
+
+def test_score_max_possible(): #perfect hands every time
+    child = pexpect.spawn('python game.py')
+
+    # roll 1
+    child.expect('.*Enter your dice roll, 5,4,3,2,1:\r\n')
+    child.sendline("1,1,1,1,1")
+    child.expect('.*Choose the result you wish to add to your scoreboard, type a number:\r\n')
+    child.sendline('1')
+    # roll 2
+    child.expect('.*Enter your dice roll, 5,4,3,2,1:\r\n')
+    child.sendline("2,2,2,2,2")
+    child.expect('.*Choose the result you wish to add to your scoreboard, type a number:\r\n')
+    child.sendline('2')
+    # roll 3
+    child.expect('.*Enter your dice roll, 5,4,3,2,1:\r\n')
+    child.sendline("3,3,3,3,3")
+    child.expect('.*Choose the result you wish to add to your scoreboard, type a number:\r\n')
+    child.sendline('3')
+    # roll 4
+    child.expect('.*Enter your dice roll, 5,4,3,2,1:\r\n')
+    child.sendline("4,4,4,4,4")
+    child.expect('.*Choose the result you wish to add to your scoreboard, type a number:\r\n')
+    child.sendline('4')
+    # roll 5
+    child.expect('.*Enter your dice roll, 5,4,3,2,1:\r\n')
+    child.sendline("5,5,5,5,5")
+    child.expect('.*Choose the result you wish to add to your scoreboard, type a number:\r\n')
+    child.sendline('5')
+    # roll 6
+    child.expect('.*Enter your dice roll, 5,4,3,2,1:\r\n')
+    child.sendline("6,6,6,6,6")
+    child.expect('.*Choose the result you wish to add to your scoreboard, type a number:\r\n')
+    child.sendline('6')
+    # roll 7 - 3 of a kind
+    child.expect('.*Enter your dice roll, 5,4,3,2,1:\r\n')
+    child.sendline("6,6,6,1,2")
+    child.expect('.*Choose the result you wish to add to your scoreboard, type a number:\r\n')
+    child.sendline('7')
+    # roll 8 - 4 of a kind
+    child.expect('.*Enter your dice roll, 5,4,3,2,1:\r\n')
+    child.sendline("6,6,6,6,2")
+    child.expect('.*Choose the result you wish to add to your scoreboard, type a number:\r\n')
+    child.sendline('8')
+    # roll 9 - full house
+    child.expect('.*Enter your dice roll, 5,4,3,2,1:\r\n')
+    child.sendline("6,6,6,2,2")
+    child.expect('.*Choose the result you wish to add to your scoreboard, type a number:\r\n')
+    child.sendline('9')
+    # roll 10 - sm-straight
+    child.expect('.*Enter your dice roll, 5,4,3,2,1:\r\n')
+    child.sendline("5,6,3,1,4")
+    child.expect('.*Choose the result you wish to add to your scoreboard, type a number:\r\n')
+    child.sendline('10')
+    # roll 11 - lg-straight
+    child.expect('.*Enter your dice roll, 5,4,3,2,1:\r\n')
+    child.sendline("5,6,3,2,4")
+    child.expect('.*Choose the result you wish to add to your scoreboard, type a number:\r\n')
+    child.sendline('11')
+    # roll 12 - chance
+    child.expect('.*Enter your dice roll, 5,4,3,2,1:\r\n')
+    child.sendline("6,6,6,6,6")
+    child.expect('.*Choose the result you wish to add to your scoreboard, type a number:\r\n')
+    child.sendline('12')
+    # roll 13 - yahtzee
+    child.expect('.*Enter your dice roll, 5,4,3,2,1:\r\n')
+    child.sendline("6,6,6,6,6")
+    child.expect('.*Choose the result you wish to add to your scoreboard, type a number:\r\n')
+    child.sendline('13')
+
+    child.expect('.*478') # getting 457 - 21 points difference?
